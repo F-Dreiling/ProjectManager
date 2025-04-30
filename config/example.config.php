96c8ae36e9c0bc11e2d6ac2c05d4554215f69465
FX Drooklyn <dr.f380@hotmail.com>
2025-04-30 11:34:32 +0200

    config

---
+++ config/example.config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'app' => [
+        'name' => 'Project Manager CRM',
+        'base_path' => '/projectmanager'
+    ],
+
+    'db' => [
+        'host' => 'localhost',
+        'database' => 'projectmanager',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4'
+    ],
+];
+
+?>
