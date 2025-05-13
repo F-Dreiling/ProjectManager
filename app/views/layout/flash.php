ef9f98ec075850fb952a5d68c047b81a7f88b892
FX Drooklyn <dr.f380@hotmail.com>
2025-05-13 16:09:59 +0200

    flash styling

--- app/views/layout/flash.php
+++ app/views/layout/flash.php
@@ -1,6 +1,6 @@
 <?php if (isset($_SESSION['success'])): ?>
     <div class="d-flex justify-content-center align-items-center">
-        <div class="alert alert-warning alert-dismissible fade show mx-auto text-center bg-success text-white" role="alert">
+        <div class="alert alert-warning alert-dismissible fade show mx-auto text-center bg-success text-white mb-4" role="alert">
             <strong><?= htmlentities($_SESSION['success']); ?></strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
@@ -9,7 +9,7 @@
 
 <?php if (isset($_SESSION['error'])) : ?>
     <div class="d-flex justify-content-center align-items-center">
-        <div class="alert alert-warning alert-dismissible fade show mx-auto text-center bg-danger text-white" role="alert">
+        <div class="alert alert-warning alert-dismissible fade show mx-auto text-center bg-danger text-white mb-4" role="alert">
             <strong><?= htmlentities($_SESSION['error']); ?></strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
