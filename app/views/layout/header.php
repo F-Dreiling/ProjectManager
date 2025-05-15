f365a6479fa858fe18215fcfe3e29a8cfe00f817
FX Drooklyn <dr.f380@hotmail.com>
2025-05-15 13:33:48 +0200

    config

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -5,7 +5,7 @@
     <title>Project Manager</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- For Localhost add BASE_PATH to the href -->
-    <link rel="stylesheet" href="/app/views/layout/style.css">
+    <link rel="stylesheet" href="<?= BASE_PATH ?>/app/views/layout/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.14.1/themes/base/jquery-ui.min.css" integrity="sha512-TFee0335YRJoyiqz8hA8KV3P0tXa5CpRBSoM0Wnkn7JoJx1kaq1yXL/rb8YFpWXkMOjRcv5txv+C6UluttluCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
