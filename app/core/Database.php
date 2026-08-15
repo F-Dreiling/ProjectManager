4a809020ee8ca8e33042674c888bd72408a5853d
FX Drooklyn <dr.f380@hotmail.com>
2026-08-15 19:05:27 +0200

    persistent PDO connection to improve performance with distributed hosts (hopefully)

--- app/core/Database.php
+++ app/core/Database.php
@@ -13,7 +13,10 @@ class Database
         try {
             $dsn = "mysql:host={$db['host']};port={$db['port']};dbname={$db['database']};charset={$db['charset']}";
             
-            $this->pdo = new PDO($dsn, $db['username'], $db['password']);
+            $this->pdo = new PDO($dsn, $db['username'], $db['password'], 
+                // Deactivate this when many users want to use the app or if DB server is on same host as the app
+                [ PDO::ATTR_PERSISTENT => true ]
+            );
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch (PDOException $e) {
